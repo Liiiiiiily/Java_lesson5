31a765d6c7100c68d6f8ba0a4dd842b86fe21bcb
Liiiiiiily <98886628+Liiiiiiily@users.noreply.github.com>
2022-11-21 10:13:40 +0400

    Add files via upload

---
+++ Java_lessons5/src/Workers.java
@@ -0,0 +1,43 @@
+public class Workers {
+    public static void main(String[] args) {
+        Person[] iSys = new Person[5];
+        iSys[0] = new Person("Ivanov Ivan", "Engineer", "ivivan@mailbox.com", "892312312", 30000, 30);
+        iSys[1] = new Person("Mariynova Maria", "Developer", "marymary@mailbox.com", "892222222",40000,44);
+        iSys[2] = new Person("Voronov Genadiy", "Secretary", "cheburator@mailbox.com", "893333333",10000,50);
+        iSys[3] = new Person("Chinov Alexey", "Cliner", "chinchin@mailbox.com", "894444444",10000,19);
+        iSys[4] = new Person("Lena Golovach", "Defense minister", "ugonnadie@mailbox.com", "892555555",3000,22);
+
+        for (Person person : iSys)
+            if (person.getAge()>40)
+                System.out.println(person);
+    }
+}
+class Person {
+    private String fullname;
+    private String post;
+    private String email;
+    private String phone;
+    private int salary;
+    private int age;
+
+    public Person(String fullname, String post, String email, String phone, int salary, int age) {
+        this.fullname = fullname;
+        this.post = post;
+        this.email = email;
+        this.phone = phone;
+        this.salary = salary;
+        this.age = age;
+    }
+    public int getAge() {
+        return age;
+    }
+    @Override
+    public String toString(){
+        return(fullname+ "\n-"
+                +post+ "\n-"
+                +email+ "\n-"
+                +phone+ "\n-"
+                +salary+ "\n-"
+                +age);
+    }
+}
